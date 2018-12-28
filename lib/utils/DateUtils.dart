067e8219fcc954dea68044d3452f45a21390b84e
Vitor <mm.vitor.21@gmail.com>
2018-12-28 17:30:24 -0300

    Fixing bug when  timezone isn't known.

--- lib/utils/DateUtils.dart
+++ lib/utils/DateUtils.dart
@@ -12,10 +12,18 @@ class DateUtils {
 
   static DateTime convertTimezone(DateTime date) {
     String tz = DateTime.now().timeZoneName;
-    if(tz == "GMT") return date;
-    if(tz == "EST") return date.add(new Duration(hours: -5));
-    int offset = tz != null ? int.parse(tz) : 0;
-    return date.add(new Duration(hours: offset));
+    if (tz == "GMT") return date;
+    if (tz == "BRST") return date.add(new Duration(hours: -2));
+    if (tz == "BRT") return date.add(new Duration(hours: -3));
+    if (tz == "EST") return date.add(new Duration(hours: -5));
+    if (tz == "PST") return date.add(new Duration(hours: -8));
+    try {
+      int offset = tz != null ? int.parse(tz) : 0;
+      return date.add(new Duration(hours: offset));
+    } catch (e) {
+      print(e);
+      return date;
+    }
   }
 
   static DateTime toDateTime(String date) {
@@ -25,20 +33,20 @@ class DateUtils {
   }
 
   static String getHour(String date) {
-    try{
+    try {
       DateTime newDate = DateTime.parse(date.replaceAll('T', ' '));
       newDate = DateUtils.convertTimezone(newDate);
       String result = "";
-      if(newDate.hour < 10) {
+      if (newDate.hour < 10) {
         result += '0';
       }
       result += "${newDate.hour}:";
-      if(newDate.minute < 10) {
+      if (newDate.minute < 10) {
         result += '0';
       }
       result += "${newDate.minute}";
       return result;
-    } catch(e) {
+    } catch (e) {
       print("getHour\n" + e.toString());
     }
     return null;
@@ -51,44 +59,49 @@ class DateUtils {
       DateTime _today = DateTime.now();
       DateTime _yesterday = _today.subtract(new Duration(days: 1));
 
-      if(compareOnlyDate(_newDate, _today)) {
+      if (compareOnlyDate(_newDate, _today)) {
         return Translations.of(context).text('today').toUpperCase();
-      } else if(compareOnlyDate(_newDate, _yesterday)) {
+      } else if (compareOnlyDate(_newDate, _yesterday)) {
         return Translations.of(context).text('yesterday').toUpperCase();
       } else {
         Locale myLocale = Localizations.localeOf(context);
-        String locale = myLocale.languageCode == "pt-BR" ? "pt_BR" : myLocale.languageCode;
+        String locale =
+            myLocale.languageCode == "pt-BR" ? "pt_BR" : myLocale.languageCode;
         await initializeDateFormatting(locale, null);
         return DateFormat.yMMMMd(locale).format(_newDate).toUpperCase();
       }
-    } catch(e) {
+    } catch (e) {
       print("displayDate\n" + e.toString());
     }
     return "";
   }
 
   static bool compareOnlyDate(DateTime fstDate, DateTime sndDate) {
-    return fstDate.day == sndDate.day && fstDate.month == sndDate.month && fstDate.year == sndDate.year;
+    return fstDate.day == sndDate.day &&
+        fstDate.month == sndDate.month &&
+        fstDate.year == sndDate.year;
   }
 
   static Future<String> displayPendencyDate(BuildContext context, String date) async {
     try {
       List<String> dates = date.split('-');
-      DateTime _newDate = new DateTime(int.parse(dates[0]), int.parse(dates[1]), int.parse(dates[2]));
+      DateTime _newDate = new DateTime(
+          int.parse(dates[0]), int.parse(dates[1]), int.parse(dates[2]));
       DateTime _today = DateTime.now();
       DateTime _yesterday = _today.subtract(new Duration(days: 1));
 
-      if(compareOnlyDate(_newDate, _today)) {
+      if (compareOnlyDate(_newDate, _today)) {
         return Translations.of(context).text('today').toUpperCase();
-      } else if(compareOnlyDate(_newDate, _yesterday)) {
+      } else if (compareOnlyDate(_newDate, _yesterday)) {
         return Translations.of(context).text('yesterday').toUpperCase();
       } else {
         Locale myLocale = Localizations.localeOf(context);
-        String locale = myLocale.languageCode == "pt-BR" ? "pt_BR" : myLocale.languageCode;
+        String locale =
+            myLocale.languageCode == "pt-BR" ? "pt_BR" : myLocale.languageCode;
         await initializeDateFormatting(locale, null);
         return DateFormat.yMMMMd(locale).format(_newDate).toUpperCase();
       }
-    } catch(e) {
+    } catch (e) {
       print("displayPendencyDate\n" + e.toString());
     }
     return "";
